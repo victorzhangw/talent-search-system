531963dd914ed84fced86122d2ebed818f2f9ea3
victorzhangw <victor.zhangw@gmail.com>
2026-03-05 10:45:12 +0800

    style(frontend): 將最小化與關閉按鈕替換為 header-minimize.svg 與 header-close.svg

--- frontend/chat-widget/src/components/ChatContainer.vue
+++ frontend/chat-widget/src/components/ChatContainer.vue
@@ -36,12 +36,10 @@
         
         <!-- Minimize & Close -->
         <button v-if="isSelectionLocked" class="icon-btn styled-circle" @click="$emit('minimize')" title="最小化至按鈕">
-            <svg class="material-icon" viewBox="0 0 15 15">
-    <path d="M6.0625 14.75V5.04167L1.85417 9.25L0 7.375L7.375 0L14.75 7.375L12.8958 9.25L8.6875 5.04167V14.75H6.0625Z" fill="black"/>
-</svg>
+            <img src="../assets/images/header-minimize.svg" class="material-icon" alt="最小化" />
         </button>
-        <button class="icon-btn styled-circle close-btn" @click="$emit('close')">
-            <svg class="material-icon" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
+        <button class="icon-btn styled-circle close-btn" @click="$emit('close')" title="關閉">
+            <img src="../assets/images/header-close.svg" class="material-icon" alt="關閉" />
         </button>
       </div>
     </div>
